11434a2506dc4ce245a72b89a1a9c806bdc6ee88
eric-s-stone <eric.stone@utah.edu>
2021-06-02 14:47:41 -0600

    Reads KDFs for n imus

---
+++ dependencies/ECG_Control/readimukdf.m
@@ -0,0 +1,36 @@
+function [imutime, accel, rategyro, maghead] = readimukdf(fname)
+
+% Reads in data saved to *.kdf file (Kalman decode filespec). This data is
+% saved to disk when running FeedbackDecode.vi.
+%
+% Version: 2021518
+% Author: Troy Tully & Eric Stone
+
+fid = fopen(fname);
+Header = fread(fid,[3,1],'single');
+Data = fread(fid,[Header(3)+ Header(1),Inf],'single');
+fclose(fid);
+
+
+
+NIPTime = [];
+NIPTime = Data(1,:);
+
+imutime = {};
+accel = {};
+rategyro = {};
+maghead = {};
+
+for index = 1:Header(2)
+    offset = (index-1)*10;
+
+    imutime{index} = Data(2+offset,:);
+
+    accel{index} = Data(3+offset:5+offset,:);
+
+    rategyro{index} = Data(6+offset:8+offset,:);
+
+    maghead{index} = Data(9+offset:11+offset,:);
+end
+
+end
